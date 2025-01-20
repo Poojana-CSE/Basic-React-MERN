d7c425f3b91fd5f562ac50481067b05c393fcff0
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-20 11:44:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Intro from './components/intro.js'
+const App = () => {
+  return (
+    <div>
+      <div className='paratag' style={{fontSize:"45px"}}>
+        <p>Hello</p>
+      </div>
+      <div><Intro/></div>
+    </div>
+  )
+}
+export default App
+
+// export default App = () => {
+//   return (
+//     <div>
+//       <p>Hello</p>
+//     </div>
+//   )
+// }
+
+
---
+++ src/components/intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const intro = () => {
+  return (
+    <div>
+      <p>This Is My Intro Page</p>
+    </div>
+  )
+}
+
+export default intro
