6eb621cedea2129e9bf875fbd62305782ca809fd
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-24 15:19:49 +0530

    Simple Routing Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
-//import Intro from './components/intro';
-import Info from './components/Info';
+// import React from 'react'
+// //import Intro from './components/intro';
+// import Info from './components/Info';
+// const App = () => {
+//   const handleclick_1=()=>{
+//     alert("Button Clicked");
+//   };
+//   return (
+//     <div>
+//         <Info handleclick_1={handleclick_1}></Info>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/login';
+
 const App = () => {
-  const handleclick_1=()=>{
-    alert("Button Clicked");
-  };
   return (
     <div>
-        <Info handleclick_1={handleclick_1}></Info>
+      <h2>This is my home page</h2>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <label>User Name: </label>
+      <input type="text" />
+      <label>Password: </label>
+      <input type="password" />
+      <button type="submit">Submit</button>
+    </div>
+  );
+};
+
+export default Login;
